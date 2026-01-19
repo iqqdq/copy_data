797c304d0b8422d843d4381b126f62acbc8612fb
iqqdq <iddcud@gmail.com>
2026-01-20 01:46:40 +0300

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
 
@@ -9,30 +8,7 @@ import 'core/core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  try {
-    await FontManager.loadFonts();
-  } catch (e) {
-    print('Failed to load font: $e');
-  }
-
   runApp(
-    MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => FileTransferService())],
-      child: App(),
-    ),
+    ChangeNotifierProvider(create: (_) => FileTransferService(), child: App()),
   );
-}
-
-class FontManager {
-  static Future<void> loadFonts() async {
-    // Явно загрузите каждый файл шрифта
-    final fontLoader = FontLoader('Morn');
-
-    fontLoader.addFont(rootBundle.load('assets/fonts/morn-light.ttf'));
-    fontLoader.addFont(rootBundle.load('assets/fonts/morn-regular.ttf'));
-    fontLoader.addFont(rootBundle.load('assets/fonts/morn-medium.ttf'));
-
-    await fontLoader.load();
-    print('Font Morn loaded successfully');
-  }
 }
