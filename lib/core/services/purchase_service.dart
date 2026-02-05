c675b9444680788e1d0873e77555f59324f530fc
iqqdq <iddcud@gmail.com>
2026-02-05 19:14:37 +0300

    save commit

--- lib/core/services/purchase_service.dart
+++ lib/core/services/purchase_service.dart
@@ -25,47 +25,42 @@ class PurchaseService {
   static PurchaseService get instance => _instance;
 
   Future init() async {
-    // TODO: RETURN
-    // await AezakmiPriceService().initialize(
-    //   apphudKey: AppConstants.apphudId,
-    //   apphudPaywallsFallbackPath: "ios/apphud_paywalls_fallback.json",
-    //   storeKitPath: "ios/Paywall.storekit",
-    //   paywallID: "paywall",
-    //   productsID: [AppConstants.weekSubID, AppConstants.weekTrialSubID],
-    // );
+    await AezakmiPriceService().initialize(
+      apphudKey: AppConstants.apphudId,
+      apphudPaywallsFallbackPath: "ios/apphud_paywalls_fallback.json",
+      storeKitPath: "ios/Paywall.storekit",
+      paywallID: "paywall",
+      productsID: [AppConstants.weekSubID, AppConstants.weekTrialSubID],
+    );
 
-    // weekProduct = AezakmiPriceService().getProduct(AppConstants.weekSubID);
-    // weekTrialProduct = AezakmiPriceService().getProduct(
-    //   AppConstants.weekTrialSubID,
-    // );
+    weekProduct = AezakmiPriceService().getProduct(AppConstants.weekSubID);
+    weekTrialProduct = AezakmiPriceService().getProduct(
+      AppConstants.weekTrialSubID,
+    );
 
-    // isSubscribed.value =
-    //     (await Apphud.hasActiveSubscription() ||
-    //     await Apphud.hasPremiumAccess());
+    isSubscribed.value =
+        (await Apphud.hasActiveSubscription() ||
+        await Apphud.hasPremiumAccess());
   }
 
-  Future purchase(
-    // {required PriceProductService priceProductService} // TODO: RETURN
-  ) async {
-    // TODO: RETURN
-    // final result = await Apphud.purchase(
-    //   product: priceProductService.apphudProduct,
-    // );
+  Future purchase({required PriceProductService priceProductService}) async {
+    final result = await Apphud.purchase(
+      product: priceProductService.apphudProduct,
+    );
 
-    // if ((result.subscription?.isActive ?? false) ||
-    //     (result.nonRenewingPurchase?.isActive ?? false || kDebugMode)) {
-    isSubscribed.value = true;
-    // }
+    if ((result.subscription?.isActive ?? false) ||
+        (result.nonRenewingPurchase?.isActive ?? false || kDebugMode)) {
+      isSubscribed.value = true;
+    }
   }
 
   Future restore() async {
-    // TODO: RETURN
-    // final result = await Apphud.restorePurchases();
+    final result = await Apphud.restorePurchases();
 
-    // if (result.subscriptions.any((element) => element.isActive) ||
-    //     result.purchases.any((element) => element.isActive) ||
-    //     kDebugMode) {
-    isSubscribed.value = true;
-    // }
+    if (result.subscriptions.any((element) => element.isActive) ||
+        result.purchases.any((element) => element.isActive) ||
+        kDebugMode) {
+      isSubscribed.value = true;
+    }
   }
 }
