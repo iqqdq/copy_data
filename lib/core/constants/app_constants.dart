f09571b8f3e2026455bd915738b2005529d38469
iqqdq <iqqddq@gmail.com>
2026-02-06 17:57:27 +0300

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,15 +1,13 @@
 abstract class AppConstants {
-  static const appID = '6758515151';
-  static const apphudId = 'app_XCpxU9b11p9PvHT2ZyF1xjRFUBVDwL';
+  static const appID = '';
+  static const apphudId = '';
 
-  static const weekSubID = 'copydata_week';
-  static const weekTrialSubID = 'copydata_week_trial';
+  static const weekSubID = '';
+  static const weekTrialSubID = '';
 
-  static const termsUrl =
-      'https://docs.google.com/document/d/1e_s5idfsgWdfhjgIKBvIR6x8UKe-z4jPiHn2jtlTa_M/edit?usp=sharing';
-  static const privacyUrl =
-      'https://docs.google.com/document/d/1SARYxb71BSUriN9oyhj-n8Qiw8JVhvoZ9tmzIYCR4XQ/edit?usp=sharing';
-  static const supportUrl = 'https://forms.gle/crscnRPxaHjZ48p79';
+  static const termsUrl = '';
+  static const privacyUrl = '';
+  static const supportUrl = '';
   static const shareUrl = 'https://apps.apple.com/app/id$appID';
   static const reviewUrl =
       'https://apps.apple.com/app/id$appID?action=write-review';
